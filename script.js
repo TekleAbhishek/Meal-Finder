a30d0fa03fe29e013a6adddfa28fac276e0cba1e
TekleAbhishek <abhishekreddy437@gmail.com>
2024-06-03 16:06:57 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ const fetchCategories = async () => {
     }
 };
 
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('mainCategories');
+    categoriesContainer.innerHTML = '';
+    const heading=document.getElementById('heading');
+    heading.innerHTML='CATEGORIES';
+    categories.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.className = 'category';
+        categoryDiv.innerHTML = `
+            <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
+            <button>${category.strCategory.toUpperCase()}</button>
+        `;
+        categoryDiv.onclick = () => fetchMealsByCategory(category.strCategory);
+        categoriesContainer.appendChild(categoryDiv);
+    });
+};
